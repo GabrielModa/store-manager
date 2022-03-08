c9e20b265b5b379610002137651cd4e26abba4d3
Gabriel Moda <gabriel-moda@hotmail.com.br>
2022-03-08 16:37:58 -0300

    test ServicesGetAll

--- test/unit/models/productsModels.test.js
+++ test/unit/models/productsModels.test.js
@@ -2,27 +2,64 @@ const sinon = require('sinon');
 const { expect } =require('chai');
 
 const connection = require('../../../models/connection');
-
-
 const productsModels = require('../../../models/productsModels');
 
 describe('Buscar todos os produtos do BD', () => {
-    describe('Quando não existir nenhum produto', () => {
-      before(() => {
-      const execute = [[]];
-      
-      sinon.stub(connection, 'execute').resolves(execute);
-      });
+  describe('Quando não existir nenhum produto', () => {
+    before(() => {
+     const execute = [[]];      
+    sinon.stub(connection, 'execute').resolves(execute);
+    });
+
+    after(() => {
+     connection.execute.restore();
+    });
 
       it('retorna um array', async () => {
         const result = await productsModels.getAll();
-        expect(result).to.be.an('array')
+        expect(result).to.be.an('array');
       });
 
       it('retorna um array vazio', async () => {
         const result = await productsModels.getAll();
         expect(result).to.be.empty;
-      });
-      
+      });      
     });
+  // describe('Quando existe produtos criados', () => {
+  //   beforea(() => {
+  //     const returnExecute = [
+  //     [
+  //       {
+  //       id: 5,
+  //       name: Celular,
+  //       quantity: 2,
+  //       },
+  //     ],
+  //   ];
+  //   sinon.stub(connection , 'execute').resolves(returnExecute);
+  //   });
+  //   after(() => {
+  //    connection.execute.restore();
+  //   });
+
+  //   it('Retorna um array', async () => {
+  //     const result = await productsModels.getAll();
+  //     expect(result).to.be.an('array');
+  //   });
+
+  //   it('Array não está vazio', async () => {
+  //     const result = await productsModels.getAll();
+  //       expect(result).to.be.not.empty;
+  //   });
+
+  //   it('Possui itens do tipo objeto', async () => {
+  //     const [item] = await productsModels.getAll();
+  //       expect(item).to.be.an('object');
+  //   })
+
+  //   it('Produtos, devem possuir "name" e "quantity"', async () => {
+  //     const [item] = await productsModels.getAll();
+  //     expect(item).include.all('name', 'quantity');
+  //   });
+  //   });
 });
---
+++ test/unit/services/productsServices.test.js
@@ -0,0 +1,28 @@
+const sinon = require('sinon');
+const { expect } =require('chai');
+
+const productsServices = require('../../../services/productsServices');
+const productsModels = require('../../../models/productsModels');
+
+describe('Buscar todos os produtos do BD', () => {
+  describe('Quando não existir nenhum produto', () => {
+    before(() => {
+     const execute = [[]];      
+    sinon.stub(productsModels, 'getAll').resolves(execute);
+    });
+
+    after(() => {
+      productsModels.getAll.restore();
+    });
+
+      it('retorna um array', async () => {
+        const result = await productsServices.getAll();
+        expect(result).to.be.an('array');
+      });
+
+      it('retorna um array vazio', async () => {
+        const result = await productsServices.getAll();
+        expect(result).to.be.empty;
+      });      
+    });
+  });
